d3ca21b143504f188c8f2d7e317ccb00607c7aa6
giftedodd <giftedoddd@gmail.com>
2024-12-22 16:03:34 +0300

    creating day3(part 2)

--- src/2024/day3/part2.py
+++ src/2024/day3/part2.py
@@ -1,71 +1,29 @@
 from pathlib import Path
-
-CLEAN_LIST = []
+import re
 
 def get_data():
     data_path = Path(__file__).parent.parent.joinpath("inputs/day3.txt")
     with data_path.open("r") as data_:
         return data_.read()
 
-def multiply():
-    result_ = 0
-    for item in CLEAN_LIST:
-        values = item.split(",")
-        result_ += int(values[0]) * int(values[1])
-    return result_
-
-def clean_up(data):
-    global CLEAN_LIST
-
-    index_ = 0
-    while True:
-        try:
-            index_ = data.index("mul", index_)
-        except ValueError:
-            break
-
-        if not data[index_ + 3] == "(":
-            index_ += 1
-            continue
-
-        next_comma = data.index(",", index_)
-        if not data[index_ + 4:next_comma].isdigit():
-            index_ += 1
-            continue
-
-        next_parentheses = data.index(")", index_)
-        if not data[next_comma + 1:next_parentheses].isdigit():
-            index_ += 1
-            continue
-
-        CLEAN_LIST.append(data[index_ + 4: next_parentheses])
-        index_ += 1
-
 def get_pattern(data):
-    state = False
-    starting_point = 0
-    while True:
-        try:
-            do_index = data.index("do()", starting_point)
-            dont_index = data.index("don't()", starting_point)
-        except ValueError:
-            break
+    pattern = r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"
+    expressions = re.findall(pattern, "".join(data))
 
-        nearest = do_index if dont_index > do_index else dont_index
+    enabled = True
+    result = 0
 
-        if starting_point > nearest:
-            state = True
+    for expression in expressions:
+        match expression[0]:
+            case "do()":
+                enabled = True
+            case "don't()":
+                enabled = False
+            case _ if enabled:
+                result += int(expression[1]) * int(expression[2])
 
-        if not state:
-            clean_up(data[starting_point:nearest])
-            starting_point += nearest
-            continue
-
-        clean_up(data[do_index:dont_index])
-        starting_point = dont_index + 1
+    return result
 
 if __name__ == '__main__':
     input_data = get_data()
-    get_pattern(input_data)
-    result = multiply()
-    print(result)
+    print(get_pattern(input_data))
